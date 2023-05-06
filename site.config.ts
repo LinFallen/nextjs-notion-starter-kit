42be124eeecb8518859a885e353bcaf09c3c2719
Wablers <62373463+LinFallen@users.noreply.github.com>
2023-05-06 17:11:16 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -14,7 +14,7 @@ export default siteConfig({
   author: 'Manaka Asumi',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'Just One More',
 
   // social usernames (optional)
   twitter: 'Manaka_Asumi',
